7bdd4dff73d5d6fab3ee0f91c34ec8f5e61d20d4
Andrey Filin <filin.aa@phystech.edu>
2023-12-17 18:15:38 +0300

    какие то unversioned

---
+++ Assets/Scripts/ItemPickUp/GameManager.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class GameManager : MonoBehaviour
+{
+    // [SerializeField]private GameObject goldUI;
+    // [SerializeField]
+    private TextMeshProUGUI txt;
+    // private int k = 0;
+    private void Start()
+    {
+
+        txt = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void Setup()
+    {
+        txt = GetComponent<TextMeshProUGUI>();
+    }
+    
+
+    public void ChangeGoldText(int goldCount)
+    {
+        if (txt)
+        {
+        txt.text = "Gold: " + goldCount;
+        }
+        else
+        {
+            txt = GetComponent<TextMeshProUGUI>();
+            ChangeGoldText(goldCount);
+        }
+    }
+}

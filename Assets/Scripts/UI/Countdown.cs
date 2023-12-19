801a754c18d8b7740cbf6f1b7fe683d01bc2b004
Vanya <iamarinichev@gmail.com>
2023-12-19 20:55:15 +0300

    updated UI and made some fixes

--- Assets/Scripts/UI/Countdown.cs
+++ Assets/Scripts/UI/Countdown.cs
@@ -1,40 +1,38 @@
 using UnityEngine.SceneManagement;
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class Countdown : MonoBehaviour
-
 {
+    enum Scenes { 
+        Menu,
+        Store,
+        SeaPort,
+        EndGame
+    }
 
-    [SerializeField]private int timeRemaining = 20;
-    [SerializeField] private string nextSceneName = "SeaPort";
+    [SerializeField] private int timeRemaining = 120;
+    [SerializeField] private Scenes nextSceneName;
+
     private TextMeshProUGUI displayTime;
     private bool isTimerStarted = false;
 
     private Color defaultColor;
 
     private Color stressColor;
-    // private Coroutine cr;
     private void Start()
     {
-        displayTime = GetComponent<Transform>().GetChild(1).gameObject.GetComponent<Transform>().GetChild(0).GetComponent<TextMeshProUGUI>();
-            // .gameObject.GetComponent<TextMeshProUGUI>();
-        // Debug.Log(GetComponent<Transform>().GetChild(1).gameObject.GetComponent<Transform>().GetChild(0));
-        // Debug.Log(displayTime);
-        // Debug.Log(timeRemaining);
+        displayTime = GetComponent<Transform>().GetChild(1).GetComponent<TextMeshProUGUI>();
 
         stressColor = new Color(255, 89, 86);
-        
+
         defaultColor = new Color(255, 223, 172);
         DisplayTime(timeRemaining);
         displayTime.overrideColorTags = true;
         displayTime.faceColor = defaultColor;
-        // Debug.Log(nextSceneName);
     }
-    
+
     void Update()
     {
 
@@ -46,27 +44,27 @@ public class Countdown : MonoBehaviour
 
         if (timeRemaining == 0)
         {
-            
-            SceneManager.LoadScene(nextSceneName);
+ 
+             Cursor.lockState = CursorLockMode.None;
+
+            SceneManager.LoadScene(nextSceneName.ToString());
         }
-        // Debug.Log(timeRemaining);
     }
-    
-    IEnumerator DoStep () {
+
+    IEnumerator DoStep()
+    {
         DisplayTime(timeRemaining);
-        while (timeRemaining > 0) {
-            yield return new WaitForSeconds (1);
+        while (timeRemaining > 0)
+        {
+            yield return new WaitForSeconds(1);
             timeRemaining--;
             DisplayTime(timeRemaining);
         }
-        // SceneManager.LoadScene("SeaPort");
     }
-    
+
     void DisplayTime(float timeToDisplay)
     {
-    //     timeToDisplay += 1;
-        // Debug.Log(Color.red);
-        int minutes = Mathf.FloorToInt(timeToDisplay / 60); 
+        int minutes = Mathf.FloorToInt(timeToDisplay / 60);
         int seconds = Mathf.FloorToInt(timeToDisplay % 60);
         displayTime.text = string.Format("{0:00}:{1:00}", minutes, seconds);
         if (seconds <= 5)

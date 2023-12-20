fa5b5c7eec15cb48eb6fa62e26306200555da438
definedhorizons <72257331+undefinedhorizons@users.noreply.github.com>
2023-12-20 16:56:14 +0300

    Added ghost

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    enum Scenes { 
+        Menu,
+        Store,
+        SeaPort,
+        EndGame
+    }
+	
+	[SerializeField] private GameObject Player;
+    [SerializeField] private float speed;
+	[SerializeField] private Scenes nextSceneName;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GhostAI());
+    }
+
+    IEnumerator GhostAI()
+    {
+        while (enabled)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, Time.deltaTime * speed);
+
+            var direction = Player.transform.position - transform.position;
+            var rotation = Quaternion.LookRotation(direction);
+            transform.rotation = rotation;
+            yield return null;
+        }
+    }
+	
+	void OnTriggerEnter(Collider col) {
+		Debug.Log(col.gameObject.tag);
+		
+		if (col.gameObject.tag == "Player") {		
+			Cursor.lockState = CursorLockMode.None;
+			SceneManager.LoadScene(nextSceneName.ToString());
+		}
+	}
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
